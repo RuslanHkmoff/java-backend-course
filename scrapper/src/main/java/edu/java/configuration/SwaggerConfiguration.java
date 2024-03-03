190a1bfa631434c488f1425f7b39a53fcbd77442
RuslanHkmoff <ruslan.khackimov2016@gmail.com>
2024-03-03 17:41:44 +0300

    added config

---
+++ scrapper/src/main/java/edu/java/configuration/SwaggerConfiguration.java
@@ -0,0 +1,17 @@
+package edu.java.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+    info = @io.swagger.v3.oas.annotations.info.Info(
+        title = "Scrapper API",
+        version = "1.0.0",
+        description = "Scrapper service",
+        contact = @Contact(name = "RuslanHkmoff", url = "https://t.me/hkmoff")
+    )
+)
+public class SwaggerConfiguration {
+}
