2c66a86de4a5673d629de4925632168f4444306c
RuslanHkmoff <ruslan.khackimov2016@gmail.com>
2024-03-24 18:45:08 +0300

    changes bot tests

--- bot/src/test/java/edu/command/ListCommandTest.java
+++ bot/src/test/java/edu/command/ListCommandTest.java
@@ -3,22 +3,24 @@ package edu.command;
 import com.pengrad.telegrambot.model.Chat;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
-import edu.java.bot.model.User;
 import edu.java.bot.model.command.ListCommand;
-import edu.java.bot.repository.UserRepository;
+import edu.java.bot.service.ScrapperService;
+import edu.java.models.response.LinkResponse;
+import edu.java.models.response.ListLinksResponse;
 import java.net.URI;
-import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class ListCommandTest {
-    private UserRepository userRepository;
+    @Mock
+    private ScrapperService scrapperService;
     @Mock
     private Update update;
     @Mock
@@ -29,13 +31,12 @@ public class ListCommandTest {
     @BeforeEach
     void init() {
         MockitoAnnotations.openMocks(this);
-        userRepository = new UserRepository();
     }
 
     @Test
     @DisplayName("test list command: command, description")
     void testBaseList() {
-        ListCommand command = new ListCommand(userRepository);
+        ListCommand command = new ListCommand(scrapperService);
         String name = "/list";
         String description = "Returns a list of tracked links";
         assertThat(command.command()).isEqualTo(name);
@@ -45,15 +46,20 @@ public class ListCommandTest {
     @Test
     @DisplayName("test list: valid command")
     void testListValidCommand() {
-        HashSet<URI> links = new HashSet<>();
-        links.add(URI.create("Link1"));
-        links.add(URI.create("Link2"));
-        userRepository.save(new User(1L, links));
-        ListCommand command = new ListCommand(userRepository);
+        ListCommand command = new ListCommand(scrapperService);
+
         when(update.message()).thenReturn(message);
         when(message.chat()).thenReturn(chat);
         when(message.text()).thenReturn("/list");
         when(chat.id()).thenReturn(1L);
+        when(scrapperService.getAllLinks(1L))
+            .thenReturn(Optional.of(new ListLinksResponse(
+                List.of(
+                    new LinkResponse(2L, URI.create("Link1")),
+                    new LinkResponse(3L, URI.create("Link2"))
+                ),
+                2
+            )));
 
         String expected = """
             Tracking links:
@@ -66,11 +72,12 @@ public class ListCommandTest {
     @Test
     @DisplayName("test List: unregistered user")
     void testTrackUnregisteredUser() {
-        ListCommand command = new ListCommand(userRepository);
+        ListCommand command = new ListCommand(scrapperService);
         when(update.message()).thenReturn(message);
         when(message.chat()).thenReturn(chat);
         when(message.text()).thenReturn("/list");
         when(chat.id()).thenReturn(1L);
+        when(scrapperService.getAllLinks(1L)).thenReturn(Optional.empty());
 
         String expected = "You need to be registered";
         assertThat(command.handle(update)).isEqualTo(expected);
