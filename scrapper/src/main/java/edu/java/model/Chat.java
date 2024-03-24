a3d97002ce4c844d94e368c8cbd43b14106df61b
RuslanHkmoff <ruslan.khackimov2016@gmail.com>
2024-03-24 18:46:08 +0300

    added jpa entity

--- scrapper/src/main/java/edu/java/model/Chat.java
+++ scrapper/src/main/java/edu/java/model/Chat.java
@@ -1,18 +1,43 @@
 package edu.java.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
+@Table(name = "chat")
 public class Chat {
+    @Id
     private Long id;
+
+    @Column(name = "created_at")
+    @ColumnDefault("NOW()")
     private OffsetDateTime createdAt;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_id")
+    private List<Subscription> subscriptions = new ArrayList<>();
+
+    public void addSubscription(Subscription subscription) {
+        subscriptions.add(subscription);
+        subscription.setChat(this);
+    }
 }
