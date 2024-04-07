7599617f3617dc76a0d2f39637447857be03357c
RuslanHkmoff <ruslan.khackimov2016@gmail.com>
2024-04-08 02:49:55 +0300

    added listener

---
+++ bot/src/main/java/edu/java/bot/listener/LinkUpdateListener.java
@@ -0,0 +1,36 @@
+package edu.java.bot.listener;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.java.bot.service.AlertService;
+import edu.java.models.request.LinkUpdateRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class LinkUpdateListener {
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private static final String DLQ_TOPIC = "messages.scrapper_dlq";
+
+    private final KafkaTemplate<String, String> deadLetterQueue;
+    private final AlertService alertService;
+
+    @SneakyThrows
+    @KafkaListener(topics = "messages.scrapper", groupId = "messages-group")
+    public void handleUpdate(@Payload String message) {
+        log.info("Received message from kafka: {}", message);
+        LinkUpdateRequest updateRequest = OBJECT_MAPPER.readValue(message, LinkUpdateRequest.class);
+        try {
+            alertService.sendAlert(updateRequest);
+        } catch (Exception e) {
+            log.info("Invalid update, send to dlq");
+            deadLetterQueue.send(DLQ_TOPIC, message);
+        }
+    }
+}
